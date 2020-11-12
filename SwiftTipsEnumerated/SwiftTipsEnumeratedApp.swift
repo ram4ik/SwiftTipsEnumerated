1468c2d21ab1071ede2f66d807e726f69be9fe6e
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-11-12 10:28:24 +0200

    Initial Commit

---
+++ SwiftTipsEnumerated/SwiftTipsEnumeratedApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTipsEnumeratedApp.swift
+//  SwiftTipsEnumerated
+//
+//  Created by ramil on 12.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTipsEnumeratedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
